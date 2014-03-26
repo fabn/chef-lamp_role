20ddc47b2a57410ef97dd1089082cf2c4cf78bc8
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-03-26 17:10:21 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'lamp_role'
+maintainer       'Fabio Napoleoni'
+maintainer_email 'f.napoleoni@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures lamp_role'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
