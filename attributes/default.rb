85b73fc1f1d68964b813258ed0728eadf53ff1a3
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-03-27 16:19:58 +0100

    Lowered limits for apache prefork, default was too high for a php installation

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,11 +5,11 @@ default[:apache][:default_modules] = %w(
 )
 
 # Prefork Attributes, tune them according node memory
-default[:apache][:prefork][:startservers]        = 16
-default[:apache][:prefork][:minspareservers]     = 16
-default[:apache][:prefork][:maxspareservers]     = 32
-default[:apache][:prefork][:serverlimit]         = 400
-default[:apache][:prefork][:maxclients]          = 400
+default[:apache][:prefork][:startservers]        = 8
+default[:apache][:prefork][:minspareservers]     = 8
+default[:apache][:prefork][:maxspareservers]     = 16
+default[:apache][:prefork][:serverlimit]         = 200
+default[:apache][:prefork][:maxclients]          = 200
 default[:apache][:prefork][:maxrequestsperchild] = 10_000
 
 # Default php modules
