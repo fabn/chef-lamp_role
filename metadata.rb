79180986dd487d2016e2a0f52e578deca8721720
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-09-29 18:18:57 +0200

    Updated metadata file

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,10 @@ description      'Installs/Configures lamp_role'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports 'ubuntu'
+# This version supports only apache >= 2.4
+supports 'ubuntu', '>= 13.10'
 
-depends 'mysql_role'
+depends 'mysql_role', '~> 0.1'
 depends 'apache2', '>= 2.0.0'
 depends 'php'
 # Optional dependencies of php_application definition
