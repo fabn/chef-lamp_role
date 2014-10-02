d0f34129ae59597b7247448c1d570ae47f7f2d9f
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-10-02 20:26:32 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'f.napoleoni@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures lamp_role'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 # This version supports only apache >= 2.4
 supports 'ubuntu', '>= 13.10'
